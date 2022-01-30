333bb51511b6e48a7cc385e66833e920ec777479
Mark Dziuban <markdziuban@gmail.com>
2022-01-30 10:16:43 -0500

    updated permissions

--- core/settings.py
+++ core/settings.py
@@ -130,9 +130,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         # 'rest_framework.permissions.IsAuthenticated',
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ]
 }
+
+# Permissions:
+# AllowAny
+# Is Authenticated
+# IsAdminUser
+# IsAuthenticatedOrReadOnly
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
